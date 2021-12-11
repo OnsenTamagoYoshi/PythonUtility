895f150ba231f3110185bfc297bb71dfbef4bc34
OnsenTamagoYoshi <yamadatarozaemon4@gmail.com>
2021-12-11 14:02:55 +0900

    指定倍数の四捨五入

---
+++ rounding.py
@@ -0,0 +1,9 @@
+def rounding(num, round):
+    # 切り捨て
+    if num % round < round / 2:
+        after_rounding = num - (num % round)
+    # 切り上げ
+    else:
+        after_rounding = num - (num % round) + round)
+    
+    return after_rounding
